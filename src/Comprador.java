145c279ede17528a21f4f5f89aa740e26e6e4e2c
midwestFaust <joaquinfelizjueves@gmail.com>
2026-05-01 16:36:15 -0400

    agregué get vuelto al expendedor (no salía de la función comprar) e hice lo del comprador

--- src/Comprador.java
+++ src/Comprador.java
@@ -3,4 +3,28 @@
  * - Constructor: Recibe Moneda, Enumeracion y Expendedor. Ejecuta la compra y retira TODO el vuelto.
  * - No puede guardar la moneda ni el objeto Producto como propiedades.
  */
-public class Comprador {}
+import Moneda.*;
+import Excepciones.*;
+
+public class Comprador {
+    private String saborConsumido;
+    private int vueltoTotal;
+
+    public Comprador(Moneda moneda, Enumeracion seleccion, Expendedor expendedor)
+            throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
+
+        //ejecutar compra y consumir producto haciendo una cadena (no se guarda como atributo)
+        this.saborConsumido = expendedor.comprarProducto(moneda, seleccion).consumir();
+
+        //retirar todo el vuelto y sumar el total
+        this.vueltoTotal = 0;
+        Moneda monedaVuelto = expendedor.getVuelto();
+        while (monedaVuelto != null) {
+            this.vueltoTotal += monedaVuelto.getValor();
+            monedaVuelto = expendedor.getVuelto();
+        }
+    }
+
+    public String getSaborConsumido() { return saborConsumido; }
+    public int getVueltoTotal() { return vueltoTotal; }
+}
